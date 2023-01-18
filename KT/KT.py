7ee280e4f8e47a5455819e598c4e73db5bb33c32
TimurDenisenko <timur.denisenko.2887@gmail.com>
2023-01-18 13:19:26 +0200

    KontrollTöö

--- KT/KT.py
+++ KT/KT.py
@@ -1,26 +1,25 @@
 from random import *
-
 #1
 print("1 Ülesanne")
-a='(\_/)'
-b="(o o)"
-c="/ | \*"
 n = input("Number 1 kuni 9 ")
 while n.isdigit()==False or (int(n)==0 or int(n)>9):
     n=input("Vale! Number 1 kuni 9! ")
 n=int(n)
-for i in range(1,int(n)+1):
-    print(a * n, end=" ")
-    print("\n")
-    print(b * n, end=" ")
-    print("\n")
-    print(c * n, end=" ")
-    print("\n")
+for i in range(1,n+1):
+    print('(\_/)', end="    ")
+print("\n",end="")
+for i in range(1,n+1):
+    print("(o o)", end="    ")
+print("\n",end="")
+for i in range(1,n+1):
+    print("/ | \*", end="   ")
+print("\n")
+
 #2
 print("2 Ülesanne")
 a=0
 L = input("Number 1 kuni 14 ")
-while L.isdigit()==False or (int(L)==0 or int(L)>14):
+while L.isdigit()==False or int(L)==0 or int(L)>14:
     L=input("Vale! Number 1 kuni 14! ")
 for i in range(0,int(L)+1):
     a+=i
@@ -32,7 +31,7 @@ print("3 Ülesanne")
 x = randint(1,100)
 for i in range(1,11):
     num=input("Number 1 kuni 100 ")
-    while num.isdigit()==False or int(num)>100:
+    while num.isdigit()==False or int(num)==0 or int(num)>100:
         num=input("Vale! Number 1 kuni 100 ")
     num=int(num)
     if num>x:
@@ -69,9 +68,11 @@ a = input("Kirjutage number ")
 while a.isdigit()==False:
     a = input("Vale! Kirjutage number ")
 a=int(a)
+if a==0:
+    x=0
 while a>0:
     n = a % 10
     a = a // 10
     x*=n
-    x1+=n
+    x1+=n 
 print(f"Summa - {x1}, Arvude korrutis - {x}")
